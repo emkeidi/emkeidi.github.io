5efdfe29914639ac2c40f24f7b2829a51aacd390
matt <97995785+emkeidi@users.noreply.github.com>
2022-04-22 01:26:50 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -21,8 +21,7 @@ export default function Home({ allPostsData }) {
       <section className={utilStyles.headingMd}>
         <p>Hello, I'm <strong>Matt</strong>. I'm learning about software development.</p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          This page is a result of the Next.js documentation.
         </p>
       </section>
 
